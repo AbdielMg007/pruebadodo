32b9033c19c1b741f9dbc9b6a127551388beb13c
abdielmg007 <abdiel.mugo@gmail.com>
2023-03-24 16:47:16 -0600

    primera version

--- app/src/main/java/com/mg/pruebadodo/MainActivity.kt
+++ app/src/main/java/com/mg/pruebadodo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.mg.pruebadodo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var redmiNote = findViewById<Button>(R.id.redmiNote)
+
+        redmiNote.setOnClickListener {
+            val intent = Intent(this, producto::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/mg/pruebadodo/producto.kt
@@ -0,0 +1,13 @@
+package com.mg.pruebadodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class producto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_producto)
+
+
+    }
+}
