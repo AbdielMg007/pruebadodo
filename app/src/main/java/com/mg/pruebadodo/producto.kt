c0219355b24f62d3cd4ed48e5e828a02bce4e297
abdiel_munoz <abdiel.mugo@gmail.com>
2023-03-30 02:34:01 -0600

    segundo borrador

--- app/src/main/java/com/mg/pruebadodo/producto.kt
+++ app/src/main/java/com/mg/pruebadodo/producto.kt
@@ -2,12 +2,36 @@ package com.mg.pruebadodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.mg.pruebadodo.databinding.ActivityProductoBinding
 
 class producto : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_producto)
+        binding = ActivityProductoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        incellFragment()
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.or -> replaceFragment(orFragment())
+                R.id.incell -> replaceFragment(incellFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+
+    }
 
 
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentMager = supportFragmentManager
+        val fragmentTrasaction = fragmentMager.beginTransaction()
+        fragmentTrasaction.replace(R.id.fragmentNav, fragment)
+        fragmentTrasaction.commit()
     }
 }
